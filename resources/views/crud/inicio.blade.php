46324b3e64621e86fd875cfb2a3e20ce215d7b2d
crisojeda58 <cris.ojeda.CO@hotmail.com>
2023-06-17 21:07:35 -0400

    InterfazInicio

---
+++ resources/views/crud/inicio.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.master')
+
+
+
+
+@section('content')
+  <div id="Principal" class="m-5">
+    <div id="titulo" class="mb-4">
+        <h1>Hola de nuevo</h1>
+    </div>
+    <div id="botones" class="d-grid gap-2 col- justify-content-md-start">
+        <a href="#" class="btn btn-dark btn-lg mb-3">Recompensa diaria</a>
+        <a href="#" class="btn btn-primary btn-lg mb-3">Jugar</a>
+        <a href="#" class="btn btn-success btn-lg mb-3">Ver Mapa regional</a>
+        <a href="#" class="btn btn-danger btn-lg mb-3">Ver pokedex</a>
+        <a href="#" class="btn btn-warning btn-lg mb-3">Ver Inventario</a>
+    </div>
+  </div>
+@endsection('content')
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,9 +3,7 @@
 
 
 <head>
-    <title>ListaUsuarios</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ProyectoWeb</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
 
@@ -21,42 +19,40 @@
 
 
 <body>
-    <div class="container-fluid " id="contenido">
-        <div class="fondo">
-            <nav class="navbar navbar-expand-lg bg-dark sticky-top">
-                <div >
-                    <!-- Compañia -->
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link mb-0 h1" href="/" style="color: white; font-size: large;">Inicio</a>
-                        </li>
-                    </ul>
+    <div class="container-fluid bg-grey">
+        <div id="fondo1" class="position-relative" id="Home" >
+            <img id="foto" class="position-absolute top-50 start-50 translate-middle rounded-circle" src="./images/fondo.jpg" alt="">
+        </div>
+        <nav class="navbar navbar-expand-lg bg-dark sticky-top">
+            <div >
+                <!-- Compañia -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link mb-0 h1" href="/" style="color: white; font-size: large;">Inicio</a>
+                    </li>
+                </ul>
 
-                </div>
-            </nav>
+            </div>
+        </nav>
 
 
-            <!--principal-->
-            <div id="Principal">
-                <main>
-                    @yield('content')
-                </main>
-            
-                <div>
-                    <footer class="footer py-3 my-4 fixed-bottom">
-                    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Inicio</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Caracteristicas</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Sobre</a></li>
-                    </ul>
-                    <p class="text-center text-body-secondary">© 2023 PokemonCompany, Inc</p>
-                    </footer>
-                </div>
+        <!--principal-->
+        <div id="Principal">
+            <main>
+                @yield('content')
+            </main>
+        
+            <div>
+                <footer class="footer py-3 my-4 fixed-bottom">
+                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Caracteristicas</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Sobre</a></li>
+                </ul>
+                <p class="text-center text-body-secondary">© 2023 PokemonCompany, Inc</p>
+                </footer>
             </div>
         </div>
-        
-
-
     </div>
     <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
