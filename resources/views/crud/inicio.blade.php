55faf6c22bccb5f308a7168cd5f9e9c1af1c319b
crisojeda58 <cris.ojeda.CO@hotmail.com>
2023-07-05 15:03:27 -0400

    05/07/2023

--- resources/views/crud/inicio.blade.php
+++ resources/views/crud/inicio.blade.php
@@ -52,22 +52,26 @@
     </div>
     <div id="lateral_izq">
       <div id="elementos">
-        <div id="titulo" class="r m-3 mb-5">
+        <div id="titulo" class="r m-2 mb-3">
             <h1 class="d-flex justify-content-center" style="color: white;">Hola de nuevo</h1>
             <h1 class="d-flex justify-content-center" style="color: white;">entrenador</h1>
         </div>
          <div class="d-flex justify-content-center mb-3">
           <a href="/dailyreward" class="btn btn-dark btn-lg" id="btn">Recompensa diaria</a>
         </div>
-        <div class="d-flex justify-content-center mb-4">
+        <div class="d-flex justify-content-center mb-3">
+            <a href="#" class="btn btn-light btn-lg" id="btn">Ruleta</a>
+        </div>
+        <div class="d-flex justify-content-center mb-3">
             <a href="/mapa" class="btn btn-success btn-lg" id="btn">Mapa Regional</a>
         </div>
-        <div class="d-flex justify-content-center mb-4">
+        <div class="d-flex justify-content-center mb-3">
             <a href="#" class="btn btn-danger btn-lg" id="btn">Pokedex</a>
         </div>
         <div class="d-flex justify-content-center">
             <a href="#" class="btn btn-warning btn-lg" id="btn">Inventario</a>
         </div>
+        
       </div>
       <img id="entrenador" src="images/Trainer.webp" alt="">
     </div>
